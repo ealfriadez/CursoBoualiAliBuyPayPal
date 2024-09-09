bfacc829b857fb19fcedbe3ab25a1cff5e6d78d8
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-09-09 16:36:19 -0500

    First commit

---
+++ src/main/java/pe/edu/unfv/config/PaypalConfig.java
@@ -0,0 +1,24 @@
+package pe.edu.unfv.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.paypal.base.rest.APIContext;
+
+@Configuration
+public class PaypalConfig {
+
+	@Value("${paypal.client-id}")
+	private String clientId;
+	@Value("${paypal.client-secret}")
+	private String clientSecret;
+	@Value("${paypal.client-mode}")
+	private String mode;
+	
+	@Bean
+	APIContext apiContext() {
+		
+		return new APIContext(clientId, clientSecret, mode);
+	}
+}
